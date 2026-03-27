9623be6b937fcdd454b4e16cfc70495c44e4b863
LengxiQwQ <lengxiowo@gmail.com>
2026-03-27 22:30:54 +0800

    Add About page

---
+++ LivePhotoStudio/Views/AboutPage.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace LivePhotoStudio.Views
+{
+    public sealed partial class AboutPage : Page
+    {
+        public AboutPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
