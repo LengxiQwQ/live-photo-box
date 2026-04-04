17985e3352c4119c6b927d8e44131471ed3d9ebd
LengxiQwQ <date200325@Gmail.com>
2026-04-05 06:34:46 +0800

    add version detection

--- LivePhotoBox/Views/AboutPage.xaml.cs
+++ LivePhotoBox/Views/AboutPage.xaml.cs
@@ -1,4 +1,8 @@
+using LivePhotoBox.Services;
 using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Reflection;
+using Windows.ApplicationModel;
 
 namespace LivePhotoBox.Views
 {
@@ -7,6 +11,20 @@ namespace LivePhotoBox.Views
         public AboutPage()
         {
             this.InitializeComponent();
+            VersionTextBlock.Text = ResourceService.Format("AboutPage_Version_Format", GetAppVersion());
+        }
+
+        private static string GetAppVersion()
+        {
+            try
+            {
+                PackageVersion version = Package.Current.Id.Version;
+                return $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+            }
+            catch (InvalidOperationException)
+            {
+                return Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "0.0.0.0";
+            }
         }
     }
 }
