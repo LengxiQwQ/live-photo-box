a6109bc8a62415844c7491571f32ddfcd6aeb276
LengxiQwQ <date200325@Gmail.com>
2026-04-10 04:39:33 +0800

    update

--- LivePhotoBox/Views/AboutPage.xaml.cs
+++ LivePhotoBox/Views/AboutPage.xaml.cs
@@ -19,37 +19,18 @@ namespace LivePhotoBox.Views
             VersionTextBlock.Text = ResourceService.Format("AboutPage_Version_Format", GetAppVersion());
         }
 
-        private async void DeveloperLinkButton_Click(object sender, RoutedEventArgs e)
-        {
-            await FilePickerService.OpenUriAsync(DeveloperUri);
-        }
+        private async void DeveloperLinkButton_Click(object sender, RoutedEventArgs e) => await FilePickerService.OpenUriAsync(DeveloperUri);
+        private async void RepositoryLinkButton_Click(object sender, RoutedEventArgs e) => await FilePickerService.OpenUriAsync(RepositoryUri);
+        private async void LicenseLinkButton_Click(object sender, RoutedEventArgs e) => await FilePickerService.OpenUriAsync(LicenseUri);
+        private async void IssueLinkButton_Click(object sender, RoutedEventArgs e) => await FilePickerService.OpenUriAsync(FeedbackService.GetIssuesUri());
 
-        private async void RepositoryLinkButton_Click(object sender, RoutedEventArgs e)
-        {
-            await FilePickerService.OpenUriAsync(RepositoryUri);
-        }
-
-        private async void LicenseLinkButton_Click(object sender, RoutedEventArgs e)
-        {
-            await FilePickerService.OpenUriAsync(LicenseUri);
-        }
-
-        private async void IssueLinkButton_Click(object sender, RoutedEventArgs e)
-        {
-            await FilePickerService.OpenUriAsync(FeedbackService.GetIssuesUri());
-        }
+        private async void ExifTool_Click(object sender, RoutedEventArgs e) => await Windows.System.Launcher.LaunchUriAsync(new Uri("https://exiftool.org/"));
+        private async void JpegTran_Click(object sender, RoutedEventArgs e) => await Windows.System.Launcher.LaunchUriAsync(new Uri("https://www.ijg.org/"));
 
         private static string GetAppVersion()
         {
-            try
-            {
-                PackageVersion version = Package.Current.Id.Version;
-                return $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
-            }
-            catch (InvalidOperationException)
-            {
-                return Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "0.0.0.0";
-            }
+            try { return Package.Current.Id.Version.Major + "." + Package.Current.Id.Version.Minor + "." + Package.Current.Id.Version.Build + "." + Package.Current.Id.Version.Revision; }
+            catch { return Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "1.0.0.0"; }
         }
     }
 }
